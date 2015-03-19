ae8dcf5bfdb4e9e564a65ba66592e1649479c4b6
Steven Watson <Kiladre@NightAir.local>
2015-03-18 18:20:01 -0700

    Studying Tutorials
    Following
    http://developer.android.com/training/basics/firstapp/index.html
    
    And getting information on Android Volley

--- app/src/main/java/com/cs315/gwtm/destinygearandguns/DisplayMessageActivity.java
+++ app/src/main/java/com/cs315/gwtm/destinygearandguns/DisplayMessageActivity.java
@@ -5,40 +5,37 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.content.Intent;
-import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends ActionBarActivity {
 
-    public final static String EXTRA_MESSAGE = "com.cs370.gwtm.destinygearandguns.MESSAGE";
+public class DisplayMessageActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-/*
-    public void sendMessage(View view) {
-        // Respond to button click
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-    }
-*/
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        //setContentView(R.layout.activity_display_message);
+
+        // Get the message from intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
+/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.activity_display_message, menu);
         return true;
     }
-
+*/
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
