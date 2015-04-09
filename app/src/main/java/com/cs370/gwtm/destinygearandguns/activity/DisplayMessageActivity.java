953f71a7cc71507644a55c0571f7f907d71039d9
Kiladre <watson.sm@gmail.com>
2015-04-08 18:29:53 -0700

    Updated
    Search on both PSN and XBL are now working. Still no parsing.
    
    EditText is now single line.
    
    Temporarily switched application launcher icon (would like to see a
    better one).

--- app/src/main/java/com/cs370/gwtm/destinygearandguns/activity/DisplayMessageActivity.java
+++ app/src/main/java/com/cs370/gwtm/destinygearandguns/activity/DisplayMessageActivity.java
@@ -3,9 +3,6 @@ package com.cs370.gwtm.destinygearandguns.activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-//import android.view.Menu;
-import android.util.Log;
-import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,7 +11,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.cs370.gwtm.destinygearandguns.R;
 import com.cs370.gwtm.destinygearandguns.utility.VolleySingleton;
 
 import org.json.JSONArray;
@@ -27,23 +23,32 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_display_message);
 
-        RequestQueue testQueue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+        RequestQueue myQueue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
 
         // Get the message from intent
         Intent intent = getIntent();
-        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        String serviceMemberName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        //String serviceMemberName = "Kiladre";
-        int membershipType = 2;
+        // serviceMemberName, Username/Account name
+        String serviceMemberName = intent.getStringExtra(MainActivity.ACCOUNT_NAME);
+
+        // membershipType, XBox Live = 1, PSN = 2.
+        int membershipType = 0;
+
+        if ( intent.getIntExtra("XBLChecked", 0) == 1 ) {
+            membershipType = intent.getIntExtra("XBLChecked", 0);
+            //Log.v("xbl checked", Integer.toString(membershipType));
+        }
+        else {
+            membershipType = intent.getIntExtra("PSNChecked", 0);
+            //Log.v("psn checked", Integer.toString(membershipType));
+        }
+
         String searchMemberUrl = "https://www.bungie.net/Platform/Destiny/SearchDestinyPlayer/"
                 + membershipType + "/" + serviceMemberName + "/";
 
 
         // Response
-        //String jsonResponse = "";
 
         // Create the text view
         final TextView textView = new TextView(this);
@@ -55,13 +60,11 @@ public class DisplayMessageActivity extends ActionBarActivity {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            String jsonResponse = "";
                             JSONArray acctInfo = response.getJSONArray("Response");
                             for(int i = 0; i < acctInfo.length(); i++) {
                                 //Log.v("Response", acctInfo.getString(0));
+                                //textView.setText( "Response" + acctInfo.getString(0) );
                                 textView.setText( "Response" + acctInfo.getString(0) );
-                                //jsonResponse += "Response" + acctInfo.getString(0);
-
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -76,10 +79,7 @@ public class DisplayMessageActivity extends ActionBarActivity {
                 }); // End JSON Object Request
 
         // Add JSON Object Request to Volley Queue
-        testQueue.add(jsonObjectRequest);
-
-        // Display response
-        //textView.setText(jsonResponse);
+        myQueue.add(jsonObjectRequest);
 
         // Set the text view as the activity layout
         setContentView(textView);
@@ -92,7 +92,7 @@ public class DisplayMessageActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.activity_display_message, menu);
         return true;   dafdf
     }
-*/
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -107,4 +107,5 @@ public class DisplayMessageActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    */
 }
